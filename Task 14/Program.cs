5fe8d19140e92d5529b27aeee55c8d3d130026fe
Даша <@DESKTOP-K4NVIE4>
2022-02-21 13:41:34 +0300

    Добавьте файлы проекта.

---
+++ Task 14/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task_14
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog = new Dog();
+            Cat cat = new Cat();
+            dog.ShowInfo();
+            cat.ShowInfo();
+            Console.ReadKey();
+        }
+
+        abstract class Animal
+
+        {
+
+            public abstract void Say();
+            public abstract string Name { get; set; }
+            public void ShowInfo()
+            {
+                Console.WriteLine(Name);
+                Say();
+            }
+              
+        }
+        class Dog:Animal
+        {
+            string name="Собака";
+            public override string Name
+            {
+                get 
+                {
+                    return name;
+                }
+                set 
+                {
+                    name = value;
+                }
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Гав");
+            }
+        }
+        class Cat : Animal
+        {
+            string name = "Кошка";
+            public override string Name
+            {
+                get
+                {
+                    return name;
+                }
+                set
+                {
+                    name = value;
+                }
+            }
+            public override void Say()
+            {
+                Console.WriteLine("Мяу");
+            }
+        }
+    }
+}
